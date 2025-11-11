00f905636f5e1cdd05d610d8f608005dbefd3374
Sergei Pankov <spankov441@gmail.com>
2025-11-11 13:13:54 +0200

    feat:finished implementing the whole challenge

--- api/nitro.config.ts
+++ api/nitro.config.ts
@@ -1,7 +1,10 @@
-import { defineNitroConfig } from "nitropack/config";
+import { defineNitroConfig } from 'nitropack/config';
 
 // https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: "latest",
-  srcDir: "server",
+   compatibilityDate: 'latest',
+   srcDir: 'server',
+   routeRules: {
+      '/api/**': { cors: true, headers: { 'access-control-allow-methods': 'GET,POST,OPTIONS,PUT,DELETE', 'access-control-allow-origin': 'http://localhost:5173', 'access-control-allow-headers': 'Content-Type,Authorization' } },
+   },
 });
--- api/server/plugins/seed.ts
+++ api/server/plugins/seed.ts
@@ -1,11 +1,12 @@
-import assets from "../../seed/assets.json";
-import comments from "../../seed/comments.json";
+import assets from '../../seed/assets.json';
+import comments from '../../seed/comments.json';
 
 export default defineNitroPlugin(async () => {
-  const store = useStorage("memory");
-  const already = await store.getItem("assets");
-  if (!already) {
-    await store.setItem("assets", assets);
-    await store.setItem("comments", comments);
-  }
+   const store = useStorage('memory');
+   const already = await store.getItem('assets');
+
+   if (!already) {
+      await store.setItem('assets', assets);
+      await store.setItem('comments', comments);
+   }
 });
