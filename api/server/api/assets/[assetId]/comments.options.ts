493c61b2cdf3e51d2778dcc764227a17d91ba411
Sergei Pankov <spankov441@gmail.com>
2025-11-11 14:06:25 +0000

    fix: fixed the update endpoints cors error on stage.

--- api/server/api/assets/[assetId]/comments.options.ts
+++ api/server/api/assets/[assetId]/comments.options.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler((event) => {
    event.node.res.statusCode = 204;
-   setHeader(event, 'Access-Control-Allow-Origin', 'http://localhost:5173');
+   setHeader(event, 'Access-Control-Allow-Origin', '*');
    setHeader(event, 'Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS');
    setHeader(event, 'Access-Control-Allow-Headers', 'Content-Type, Authorization');
    event.node.res.end();
